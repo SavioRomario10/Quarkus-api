ed5daef6cdfb54b0c6c86cff5a898092270a7dd4
Sávio Romário <savioromario10@gmail.com>
2026-07-06 10:48:28 -0300

    first commit

---
+++ src/main/java/io/savioromario10/quarkussocial/rest/UserResource.java
@@ -0,0 +1,21 @@
+package io.savioromario10.quarkussocial.rest;
+
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+import io.savioromario10.quarkussocial.rest.dto.CreateUserRequest;
+
+@Path("/users")
+public class UserResource {
+
+  @POST
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response createUser(CreateUserRequest userRequest) {
+    return Response.ok().build();
+  }
+}
